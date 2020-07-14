a08619814836c4bc7ce5236a94e2485ac3ea7ad0
eastern-kapil <k.fatale@easternenterprise.com>
2020-07-14 14:32:11 +0530

    updated Rate to make AdditionalGuestAmounts optional

--- otamodel/common.go
+++ otamodel/common.go
@@ -61,28 +61,28 @@ type BookingRule struct {
 }
 
 type Rate struct {
-	BaseByGuestAmts        BaseByGuestAmts        `json:"BaseByGuestAmts"`
-	OriginalData           map[string]interface{} `json:"original_data,omitempty"`
-	AdditionalGuestAmounts AdditionalGuestAmounts `json:"AdditionalGuestAmounts"`
-	CancelPolicies         *CancelPolicies        `json:"CancelPolicies,omitempty"`
-	MealsIncluded          *MealsIncluded         `json:"MealsIncluded,omitempty"`
-	Fees                   *Fees                  `json:"Fees,omitempty"`
-	GuaranteePolicies      *GuaranteePolicies     `json:"GuaranteePolicies,omitempty"`
-	RateDescription        *RateDescription       `json:"RateDescription,omitempty"`
-	NumberOfUnits          string                 `json:"NumberOfUnits,omitempty"`
-	Mon                    string                 `json:"Mon,omitempty"`
-	Tue                    string                 `json:"Tue,omitempty"`
-	Weds                   string                 `json:"Weds,omitempty"`
-	Thur                   string                 `json:"Thur,omitempty"`
-	Fri                    string                 `json:"Fri,omitempty"`
-	Sat                    string                 `json:"Sat,omitempty"`
-	Sun                    string                 `json:"Sun,omitempty"`
-	Start                  string                 `json:"Start"`
-	End                    string                 `json:"End"`
-	InvTypeCode            string                 `json:"InvTypeCode,omitempty"`
-	MaxLOS                 string                 `json:"MaxLOS,omitempty"`
-	MinLOS                 string                 `json:"MinLOS,omitempty"`
-	RateTimeUnit           string                 `json:"RateTimeUnit,omitempty"`
+	BaseByGuestAmts        BaseByGuestAmts         `json:"BaseByGuestAmts"`
+	OriginalData           map[string]interface{}  `json:"original_data,omitempty"`
+	AdditionalGuestAmounts *AdditionalGuestAmounts `json:"AdditionalGuestAmounts"`
+	CancelPolicies         *CancelPolicies         `json:"CancelPolicies,omitempty"`
+	MealsIncluded          *MealsIncluded          `json:"MealsIncluded,omitempty"`
+	Fees                   *Fees                   `json:"Fees,omitempty"`
+	GuaranteePolicies      *GuaranteePolicies      `json:"GuaranteePolicies,omitempty"`
+	RateDescription        *RateDescription        `json:"RateDescription,omitempty"`
+	NumberOfUnits          string                  `json:"NumberOfUnits,omitempty"`
+	Mon                    string                  `json:"Mon,omitempty"`
+	Tue                    string                  `json:"Tue,omitempty"`
+	Weds                   string                  `json:"Weds,omitempty"`
+	Thur                   string                  `json:"Thur,omitempty"`
+	Fri                    string                  `json:"Fri,omitempty"`
+	Sat                    string                  `json:"Sat,omitempty"`
+	Sun                    string                  `json:"Sun,omitempty"`
+	Start                  string                  `json:"Start"`
+	End                    string                  `json:"End"`
+	InvTypeCode            string                  `json:"InvTypeCode,omitempty"`
+	MaxLOS                 string                  `json:"MaxLOS,omitempty"`
+	MinLOS                 string                  `json:"MinLOS,omitempty"`
+	RateTimeUnit           string                  `json:"RateTimeUnit,omitempty"`
 }
 
 type BaseByGuestAmt struct {
